b68a49e8c45f10389bbf619a5a0b69206141e6b2
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2016-05-23 15:20:32 +0200

    exemple_06 time loop: add grep

--- exemples/01-programmation-noyau/exemple_06.c
+++ exemples/01-programmation-noyau/exemple_06.c
@@ -16,17 +16,16 @@ static int __init exemple_init (void)
 {
 	struct timeval  tv_jif, tv_tod;
 	struct timespec ts_ckt, ts_tod;
-  int i=0;
+  int i;
   for(i=0;i<1024;i++)
   {
 	jiffies_to_timeval(jiffies, & tv_jif);
 	do_gettimeofday(& tv_tod);
 	ts_ckt = current_kernel_time();
 	getnstimeofday(& ts_tod);
-     if(i==0||i==999)
-     {
+
 	printk(KERN_INFO  "%s - %s():\n", THIS_MODULE->name, __FUNCTION__);
-	printk(KERN_INFO  "%s - i=%d:\n", THIS_MODULE->name, i);
+    printk(KERN_INFO  "i=%04d:\n", i);
 	printk(KERN_INFO "tv_jif.tv_sec = %ld, tv_jif.tv_usec = %ld\n",
 	                  tv_jif.tv_sec, tv_jif.tv_usec);
 	printk(KERN_INFO "tv_tod.tv_sec = %ld, tv_tod.tv_usec = %ld\n",
@@ -35,7 +34,6 @@ static int __init exemple_init (void)
 	                  ts_ckt.tv_sec, ts_ckt.tv_nsec);
 	printk(KERN_INFO "ts_tod.tv_sec = %ld, ts_tod.tv_nsec = %ld\n",
 	                  ts_tod.tv_sec, ts_tod.tv_nsec);
-     }
   }//i loop
 	return 0;
 }
