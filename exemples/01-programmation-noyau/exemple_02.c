3a1b61ec145d08b0c640cd6bae4778172f847df9
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2016-05-23 11:53:15 +0200

    exemple_02 - label and input

--- exemples/01-programmation-noyau/exemple_02.c
+++ exemples/01-programmation-noyau/exemple_02.c
@@ -10,19 +10,19 @@
 
 	#include <linux/module.h>
 
-	static int input = 0;
-	module_param(input, int, 0644);
-	MODULE_PARM_DESC(input, "The input channel number.");
+	static int input=0;
+	module_param(input, int, 0644);//,,acces mod
+	MODULE_PARM_DESC(input, "The input channel number.");//for modinfo
 
 
-	static char * exemple_label = THIS_MODULE->name;
-	module_param_named(label, exemple_label, charp, 0644);
+	static char * example_label = THIS_MODULE->name;
+	module_param_named(label, example_label, charp, 0644);//name=label
 	MODULE_PARM_DESC(label, "The label to use during communication.");
 
 
 	static int param_table[8];
 	static int nb_items;
-	module_param_array(param_table, int, & nb_items, 0644);
+	module_param_array(param_table, int, &nb_items, 0644);
 
 
 static int __init exemple_init (void)
@@ -30,7 +30,7 @@ static int __init exemple_init (void)
 	int i;
 
 	printk(KERN_INFO "%s: input = %d\n", THIS_MODULE->name, input);
-	printk(KERN_INFO "%s: exemple_label = %s\n", THIS_MODULE->name, exemple_label);
+	printk(KERN_INFO "%s: example_label = %s\n", THIS_MODULE->name, example_label);
 	printk(KERN_INFO "%s: nb_items = %d\n", THIS_MODULE->name, nb_items);
 	printk(KERN_INFO "%s: Table = ", THIS_MODULE->name);
 	for (i = 0; i < nb_items; i ++)
